e81ee6048d19fbf12f9d59ff6c69dd279c8a8ec9
serdar kaplan <ozserdarkaplan66@gmail.com>
2025-10-24 03:24:58 +0300

    Update BaseGamePass.sol

--- contracts/BaseGamePass.sol
+++ contracts/BaseGamePass.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.2/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.0.2/contracts/access/Ownable.sol";
 
 contract BaseGamePass is ERC721, Ownable {
     uint256 private _tokenIdCounter;
-    uint256 public constant MAX_SUPPLY = 6666; // Toplam 6666 NFT
+    uint256 public constant MAX_SUPPLY = 6666;
 
     constructor() ERC721("BaseGamePass", "BGP") Ownable(msg.sender) {
         _tokenIdCounter = 0;
@@ -24,6 +24,10 @@ contract BaseGamePass is ERC721, Ownable {
 
     function totalSupply() external view returns (uint256) {
         return _tokenIdCounter;
+    }
+
+    function _exists(uint256 tokenId) internal view returns (bool) {
+        return _ownerOf(tokenId) != address(0);
     }
 
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
